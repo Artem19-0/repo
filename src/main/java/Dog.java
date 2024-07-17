a6e12459762ceb9f19e4c72fd6788d7444e7e0cf
Artem Klevko <klevko4@mail.ru>
2024-07-17 10:46:33 +0300

    Update

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -22,3 +22,43 @@ public class Dog {
         return Objects.hash(age, name);
     }
 }
+
+
+
+ class Animal {
+
+    protected int name;
+    void sound() {
+        System.out.println("animal can make a sound");
+    }
+}
+
+class Dog2 extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Woof " + name);
+    }
+}
+
+class Duck extends Animal {
+    //@Override
+    void sound() {
+        System.out.println("кря-кря");
+    }
+}
+
+class TestPolymorphism {
+    public static void main(String[] args) {
+        Animal myAnimal = new Animal();
+        Animal myDog = new Dog2();
+        Animal myCat = new Duck();
+
+//        myAnimal.sound();
+//        myDog.sound();
+//        myCat.sound();
+
+        // faeture
+    }
+}
+
+
