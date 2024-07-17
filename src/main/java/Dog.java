6e697e507a149547dd2813babeebae59b86f392b
Artem Klevko <klevko4@mail.ru>
2024-07-17 10:38:46 +0300

    Test commit from MAC

---
+++ src/main/java/Dog.java
@@ -0,0 +1,24 @@
+import java.util.Objects;
+
+public class Dog {
+    private int age;
+    private String name;
+
+    public Dog(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dog dog = (Dog) o;
+        return age == dog.age && Objects.equals(name, dog.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(age, name);
+    }
+}
